f277ab69eed3465d2589033c057bcf6d291af475
elessarbaz <darmen89@yandex.ru>
2017-01-19 16:59:46 +0600

    Вёрстка

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,37 @@ $( window ).on( "load", function() {
     percentPosition: true
   });
 
-// make all grid-items draggable
+  // make all grid-items draggable
   $grid.find('.grid-item').each( function( i, gridItem ) {
     var draggie = new Draggabilly( gridItem );
     // bind drag events to Packery
     $grid.packery( 'bindDraggabillyEvents', draggie );
   });
+
+
+  const data = [
+    ['1', 500],
+    ['2', 450],
+    ['3', 400],
+    ['4', 300],
+    ['5', 250]
+  ];
+  const options = {
+    block: {
+      dynamicHeight: true,
+      highlight: true,
+      fill: {
+        type : 'gradient'
+      }
+    },
+    chart: {
+      curve : {
+        enabled : true
+      }
+    }
+  };
+
+  const chart = new D3Funnel('#funnel');
+  chart.draw(data, options);
 
 });
